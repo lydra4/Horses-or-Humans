c18bb9c12a9b79dd8044a9a29bc43ca490c23af7
Song Yi <lydra4@gmail.com>
2026-05-06 09:34:51 +0800

    chore: remove arugments in setup logging of app entry script

--- src/app.py
+++ src/app.py
@@ -1,24 +1,24 @@
 import logging
-import os
 
 import gradio as gr
 import hydra
-import omegaconf
 import torch
+from omegaconf import DictConfig
+
 from inference.inference_pipeline import InferencePipeline
 from utils.general_utils import setup_logging
 from utils.seed_utils import fix_seed
 
 
-@hydra.main(version_base=None, config_path="../conf", config_name="inference.yaml")
-def main(cfg: omegaconf.DictConfig):
+@hydra.main(
+    version_base=None,
+    config_path="../conf",
+    config_name="inference.yaml",
+)
+def main(cfg: DictConfig):
     logger = logging.getLogger(__name__)
-    logging.info("Setting up logging configuration.\n")
-    setup_logging(
-        logging_config_path=os.path.join(
-            hydra.utils.get_original_cwd(), "conf", "logging.yaml"
-        )
-    )
+    logging.info("Setting up logging configuration.")
+    setup_logging()
 
     fix_seed(seed=cfg.environ.seed)
 
